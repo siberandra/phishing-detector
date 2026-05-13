eb16d8c79e81db2889ead98bb0feba88cceacc44
Vicky Chandra <vickyc.job@gmail.com>
2026-05-13 21:22:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,21 +18,7 @@
 # Contact: vickyc.job@gmail.com
 # ============================================================
 
-# ============================================================
-# Phishing Email Detection System
-# Copyright (C) 2026 Vicky Chandra
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# Contact: vickyc.job@gmail.com
-# ============================================================
+
 
 import os
 import re
